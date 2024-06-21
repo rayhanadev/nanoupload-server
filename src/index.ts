f6d24e0048a54af0c69f00d8840e86288ab6aa18
ray <me@rayhanadev.com>
2024-06-21 00:03:37 -0500

    feat: setup routes

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,22 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.post("/nano/create", (c) => {});
+
+app.get("/l/:id", (c) => {
+  // route for x content
+});
+
+app.get("/t/:id", (c) => {
+  // route for x content
+});
+
+app.get("/i/:id", (c) => {
+  // route for x content
+});
+
+app.get("/f/:id", (c) => {
+  // route for x content
+});
+
 export default app;
